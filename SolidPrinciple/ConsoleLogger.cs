beb5c99fe0d8b22773cd364d4dc33590b8ed0af1
Hareendran Nair Meena Kumari <s.nair.meena.kumari@accenture.com>
2023-02-08 13:34:57 +0200

    Single Responsibility

---
+++ SolidPrinciple/ConsoleLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace SolidPrinciple
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SolidPrinciple/FilePolicySource.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciple
+{
+    public class FilePolicySource
+    {
+        public string GetPolicyFromSource()
+        {
+            return File.ReadAllText("policy.json");
+        }
+    }
+}
